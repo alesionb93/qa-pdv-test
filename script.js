adcaa09457811aca4ed6bdb27036e30ae16e041c
alesionb93 <alesio.barreiro@eyemobile.com.br>
2026-04-09 09:45:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("PDV QA iniciado");
+function login() {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+  const errorMsg = document.getElementById("error-msg");
+
+  if (username === "candidato" && password === "Candidato.22") {
+    window.location.href = "home.html";
+  } else {
+    errorMsg.style.display = "block";
+  }
+}
